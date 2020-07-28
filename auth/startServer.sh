fc149a1d1334ad773251986917dffeb3c31e01f9
Bastian Bührig <mail@bastian-buehrig.de>
2020-07-29 00:24:15 +0200

    facebook, google and apple keys added

--- auth/startServer.sh
+++ auth/startServer.sh
@@ -7,10 +7,12 @@ export DB_NAME="ch3ck1n"
 export DB_USER="ch3ck1n_user"
 export DB_PASSWORD="==>"
 
-export P_FACEBOOK_KEY="fb_key"
-export P_FACEBOOK_SECRET="fb_secret"
-export P_GPLUS_KEY="gplus_key"
-export P_GPLUS_SECRET="gplus_secret"
+export P_FACEBOOK_KEY=""
+export P_FACEBOOK_SECRET=""
+export P_GPLUS_KEY=""
+export P_GPLUS_SECRET=""
+export P_APPLE_KEY=""
+export P_APPLE_SECRET=""
 
 # static part plus dynamic replacement for the activaation token
 export ACTIVATION_URL="https://checkin.chckr.de/auth/activation/%s"
