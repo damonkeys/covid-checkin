48736039cd39e064d2b89f5e7702e4b5e3f2f275
Bastian Bührig <mail@bastian-buehrig.de>
2020-08-05 16:56:32 +0200

    simplified migration

--- auth/dbmate/db/migrations/20200805131555_replace_activation_columns.sql
+++ auth/dbmate/db/migrations/20200805131555_replace_activation_columns.sql
@@ -1,29 +1,11 @@
 -- migrate:up
 ALTER TABLE users
-ADD COLUMN activation_token varchar(36) NULL DEFAULT NULL,
-ADD COLUMN activation_token_creation timestamp NULL DEFAULT NULL,
-ADD COLUMN active_since timestamp NULL DEFAULT NULL;
-
-UPDATE users set activation_token = activationToken;
-UPDATE users set activation_token_creation = activationTokenCreation;
-UPDATE users set active_since=activeSince;
-
-ALTER TABLE users
-DROP COLUMN activationToken,
-DROP COLUMN activationTokenCreation,
-DROP COLUMN activeSince;
+CHANGE COLUMN `activationToken` `activation_token` varchar(36),
+CHANGE COLUMN `activationTokenCreation` `activation_token_creation` timestamp,
+CHANGE COLUMN `activeSince` `active_since` timestamp;
 
 -- migrate:down
 ALTER TABLE users
-ADD COLUMN activationToken varchar(36) NULL DEFAULT NULL,
-ADD COLUMN activationTokenCreation timestamp NULL DEFAULT NULL,
-ADD COLUMN activeSince timestamp NULL DEFAULT NULL;
-
-UPDATE users set activationToken = activation_token;
-UPDATE users set activationTokenCreation = activation_token_creation;
-UPDATE users set activeSince = active_since;
-
-ALTER TABLE users
-DROP COLUMN activation_token,
-DROP COLUMN activation_token_creation,
-DROP COLUMN active_since;
+CHANGE COLUMN `activation_token` `activationToken` varchar(36),
+CHANGE COLUMN `activation_token_creation` `activationTokenCreation` timestamp,
+CHANGE COLUMN `active_since` `activeSince` timestamp;
