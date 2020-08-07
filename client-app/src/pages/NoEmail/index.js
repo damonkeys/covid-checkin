f079e3b5ade53b759f01c970ab964a1ef346c08a
Bastian Bührig <mail@bastian-buehrig.de>
2020-08-07 15:05:20 +0200

    Added some placeholder-pages for activation

---
+++ client-app/src/pages/NoEmail/index.js
@@ -0,0 +1,26 @@
+// @flow
+import React from 'react';
+import { Page, Navbar, Block } from 'framework7-react';
+import i18n from '../../components/i18n.js';
+
+
+export default () => (
+  <Page>
+    <Navbar color="pink" title="ch3ck1n"/>
+    <Block strong style={{ textAlign: "center" }}>
+      <h1>{i18n.t('explanation.bummer')}</h1>
+      <h3>{i18n.t('explanation.butcannotusemc')}</h3>
+      <h3>{i18n.t('explanation.apologizeemailrequired')}</h3>
+    </Block>
+    <Block inset>
+    <h4>{i18n.t('explanation.mailrecievingprohibited')}</h4>
+    <p>{i18n.t('explanation.changemind')}</p>
+      <ol>
+        <li>{i18n.t('explanation.repair.step1')}</li>
+        <li>{i18n.t('explanation.repair.step2')}</li>
+        <li>{i18n.t('explanation.repair.step3')}</li>
+        <li>{i18n.t('explanation.repair.step4')}</li>
+      </ol>
+    </Block>
+  </Page>
+);
