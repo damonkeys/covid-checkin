c02d0cc965422dd951c218d0f25f00430aded262
Bastian Bührig <mail@bastian-buehrig.de>
2020-08-07 15:05:20 +0200

    Added some placeholder-pages for activation

--- client-app/src/js/routes.js
+++ client-app/src/js/routes.js
@@ -1,6 +1,9 @@
 
 import HomePage from '../pages/home.jsx';
 import Privacy from '../pages/Privacy';
+import NoEmail from '../pages/NoEmail';
+import ActivationMissing from '../pages/Activation/missing';
+import ActivationStart from '../pages/Activation/start';
 import NotFoundPage from '../pages/NotFoundPage';
 
 var routes = [
@@ -10,6 +13,15 @@ var routes = [
   }, {
     path: '/privacy',
     component: Privacy,
+  }, {
+    path: '/NoEmail',
+    component: NoEmail
+  }, {
+    path: '/activation/missing',
+    component: ActivationMissing
+  }, {
+    path: '/activation/start',
+    component: ActivationStart
   }, {
     path: '(.*)',
     component: NotFoundPage
---
+++ client-app/src/pages/Activation/start.js
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react';
+import { Page, Navbar, Block } from 'framework7-react';
+
+
+export default () => (
+  <Page>
+    <Navbar color="pink" title="ch3ck1n"/>
+    <Block strong style={{ textAlign: "center" }}>
+      <h1>4c71v4710n!</h1>
+      <h3>welcome to your activation! :)</h3>
+    </Block>
+  </Page>
+);
