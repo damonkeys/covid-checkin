cb2af5918132985220a76727848b0d1e63559a07
Sven <sho1975@gmail.com>
2020-10-18 15:31:57 +0200

    Improve Checkin: search for biz before checkin form is loaded (#65)
    * Unrelated: Improved typing
    
    * MINOR: reformat and rename
    
    * Minor: Folder renamed matching the component
    
    * MINOR: Component name clarification
    
    * introduced more i18n & bigger button
    
    * moved checkin form out of business & removed unneccesary error message
    
    * clean up of the components and beginning of extraction of session handling into separate module not a SessionComponent anymore (Reuse of Composition over Inheritance)
    
    * Use props instead of session call and remove class remainings
    
    * use empty array as 2nd param to call useEffect only once
    
    * removed/ changed things that cause warnings in the console
    
    * session handling: cookie is saved automatically in updateSession() - so nothing needed here
    
    * removed ignorable comment
    
    * MINOR: formatting
    
    * CR deleted binary. No idea how this happened?
    
    * Minor: Formatting
    
    * Prevent button from being clicked without 5 char checkr Code
    
    * Remove session fetch/ replace it with session props
    
    * Transform into functional component
    
    * refactoring: rest call and session props
    
    * improved check on checkin button
    
    * MINOR: better naming
    
    * new translations for messages during checkin
    
    * mostly functionality related to https://github.com/damonkeys/ch3ck1n/pull/65
    
    * CR minlength not neccessary
    
    * CR moved component out of pages folder

--- client-app/src/components/FindBusinessByCheckrCodeForm/index.js
+++ client-app/src/components/FindBusinessByCheckrCodeForm/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Block,
     Button,
@@ -7,9 +7,11 @@ import {
     Col,
     Row
 } from 'framework7-react';
-import i18n from '../../components/i18n.js';
+import type { BusinessData } from '../../js/types';
+import checkHTTPError from '../../modules/checkHTTPError';
+import { useTranslation } from 'react-i18next';
 
-type Props = {
+type FindBusinessByCheckrCodeFormProps = {
 }
 
 /**
@@ -17,29 +19,89 @@ type Props = {
  * The user can input a 5-letter "checkr"-Code to find the business she is looking for (to checkin).
  * After touching the button she is redirected to the business page on which she can checkin.
  */
-const FindBusinessByCheckrCodeForm = (props: Props) => {
-    const [chckrCode, setChckrCode] = useState('');
+const FindBusinessByCheckrCodeForm = (props: FindBusinessByCheckrCodeFormProps) => {
+    const [t] = useTranslation();
+    const initialInfoText = t("checkin.initialInfo");
+    const [chckrCode: string, setChckrCode] = useState('');
+    const [businessData: BusinessData, setBusinessData] = useState({});
+    const [infoMessage: string, setInfoMessage] = useState(initialInfoText);
+
+    useEffect(() => {
+        if (chckrCode.length < 5) {
+            return;
+        }
+        fetch('/biz/business/' + chckrCode, {
+            method: 'GET'
+        })
+            .then(checkHTTPError)
+            .then((response: BusinessData) => {
+                setInfoMessage(t("checkin.validCode"));
+                response.formattedAddress = formatAddress(response);
+                setBusinessData(response);
+            })
+            .catch((error: number) => {
+                setInfoMessage(t("checkin.invalidCode"));
+                setBusinessData({});
+            });
+    }, [chckrCode, t]);
+
+    // this function extracts out of BusinessData-response all address-data and returns an
+    // address-string for showing.
+    //
+    // It returns this format:
+    // <street>, <city>
+    //
+    // if there es one element undefined it won't be in the result string.
+    const formatAddress = (businessData: BusinessData): string => {
+        var address: string[] = [];
+        address.push(businessData.street || '');
+        address.push(businessData.city || '');
+        address = address.filter(function (el) {
+            return el !== undefined && el.trim() !== '';
+        });
+        return address.join(', ');
+    };
+
+    const checkinPossible = (): boolean => {
+        return (chckrCode.length < 5 || businessData.uuid === undefined);
+    };
+
+    const runInputCleared = (event: any) => {
+        setInfoMessage(initialInfoText);
+    };
 
     return <Block strong>
-        {i18n.t('dashboard.explanation')}<br />
-        <h3 className="text-align-center">{i18n.t('dashboard.chckr-code')}</h3>
+        {t('dashboard.explanation')}<br />
+        <h3 className="text-align-center">{t('dashboard.chckr-code')}</h3>
         <Row>
             <Col width="15"></Col>
             <Col width="70">
-                <h1 >
+                <h1 style={{ textAlign: 'center' }}>
                     <Input
-                    inputStyle={{ textAlign: 'center' }}
-                    clearButton id="chckr-code"
-                    label={i18n.t('dashboard.chckr-code')}
-                    type="text"
-                    maxlength="5"
-                    minlength="2"
-                    placeholder={i18n.t('dashboard.chckr-code-placeholder')}
-                    onInput={(value) => {setChckrCode(value.currentTarget.value);}}>
+                        inputStyle={{ textAlign: 'center' }}
+                        clearButton id="chckr-code"
+                        label={t('dashboard.chckr-code')}
+                        type="text"
+                        maxlength="5"
+                        placeholder={t('dashboard.chckr-code-placeholder')}
+                        onInput={(value) => { setChckrCode(value.currentTarget.value); }}
+                        validate={false}
+                        info={infoMessage}
+                        onInputClear={runInputCleared}
+                        onInputEmpty={runInputCleared}
+                    >
                     </Input>
                 </h1>
                 <br />
-                <Button large raised fill iconF7="checkmark" href={'/checkin/' + chckrCode}>{i18n.t('business.findBizAndCheckin')}</Button>
+                <Button
+                    large
+                    raised
+                    fill
+                    iconF7="checkmark"
+                    href={'/checkin/' + chckrCode}
+                    preventRouter={checkinPossible()}
+                    routeProps={{ businessData: businessData }}>{t('business.findBizAndCheckin')}
+                </Button>
             </Col>
             <Col width="15"></Col>
         </Row>
--- client-app/src/pages/Home/index.js
+++ client-app/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Block,
     BlockTitle,
@@ -14,60 +14,63 @@ import {
 } from 'framework7-react';
 import { f7 } from 'framework7-react';
 import i18n from '../../components/i18n.js';
-import SessionComponent from '../../components/SessionComponent/index';
 import Logins from '../../components/Logins/index';
 import FindBusinessByCheckrCodeForm from '../../components/FindBusinessByCheckrCodeForm/index.js';
 import Account from '../../components/Account';
+import { updateSession, getSession } from '../../modules/session';
+;
 
-export default class Home extends SessionComponent {
-    render() {
-        return (
-            <Page colorTheme="pink">
-                <Navbar color="pink" className="navbar-main">
-                    <NavLeft>
-                        {( f7.views.main.router.history.length <= 1) ? null :
-                            (
-                                <NavLeft backLink={i18n.t('basic.back')}></NavLeft>
-                            )
-                        }
-                    </NavLeft>
+const Home = () => {
 
-                    <NavTitle>{i18n.t('basic.appname')}</NavTitle>
-                </Navbar>
-                
-                <Toolbar tabbar labels bottom>
-                    <Link tabLink="#home-ch3ck1n" iconIos="f7:checkmark_shield" iconAurora="f7:checkmark_shield" iconMd="material:verified_user" tabLinkActive>ch3ck1n</Link>
-                    <Link tabLink="#home-account" iconIos="f7:person_crop_circle" iconAurora="f7:person_crop_circle" iconMd="material:account_circle">Account</Link>    
-                </Toolbar>
-                
-                <Tabs swipeable>
-                    <Tab id="home-ch3ck1n" tabActive>
-                        <BlockTitle large className="text-align-center">{i18n.t('basic.appname')}</BlockTitle>
-                        { (this.state.useronline || !this.state.connected) ? null :
-                            (
-                                <Block className="text-align-center">
-                                    {i18n.t('signin.explanation-short')}
-                                    <Logins compact={true}></Logins>
-                                </Block>
-                            )
-                        }
+    const [session: Session, setSession] = useState({});
 
-                        { !this.state.connected ? null :
-                            (
-                                <FindBusinessByCheckrCodeForm></FindBusinessByCheckrCodeForm>
-                            )
-                        }
-                    </Tab>
+    useEffect(() => {
+        updateSession();
+        setSession(getSession());
+    }, [session]);
 
-                    <Tab id="home-account">
-                        { !this.state.connected ? null :
-                            (
-                                <Account session={this.state}></Account>
-                            )
-                        }
-                    </Tab>
-                </Tabs>
-            </Page>            
-        );
-    }
+    return (<Page colorTheme="pink">
+        <Navbar color="pink" className="navbar-main">
+            <NavLeft>
+                {(f7.views.main.router.history.length <= 1) ? null :
+                    (
+                        <NavLeft backLink={i18n.t('basic.back')}></NavLeft>
+                    )
+                }
+            </NavLeft>
+
+            <NavTitle>{i18n.t('basic.appname')}</NavTitle>
+        </Navbar>
+
+        <Toolbar tabbar labels bottom>
+            <Link tabLink="#home-ch3ck1n" iconIos="f7:checkmark_shield" iconAurora="f7:checkmark_shield" iconMd="material:verified_user" tabLinkActive>ch3ck1n</Link>
+            <Link tabLink="#home-account" iconIos="f7:person_crop_circle" iconAurora="f7:person_crop_circle" iconMd="material:account_circle">Account</Link>
+        </Toolbar>
+
+        <Tabs swipeable>
+            <Tab id="home-ch3ck1n" tabActive>
+                <BlockTitle large className="text-align-center">{i18n.t('basic.appname')}</BlockTitle>
+                {!session.useronline ? null :
+                    (
+                        <Block className="text-align-center">
+                            {i18n.t('signin.explanation-short')}
+                            <Logins compact={true}></Logins>
+                        </Block>
+                    )
+                }
+                <FindBusinessByCheckrCodeForm></FindBusinessByCheckrCodeForm>
+            </Tab>
+
+            <Tab id="home-account">
+                {session.useronline ? null :
+                    (
+                        <Account session={session}></Account>
+                    )
+                }
+            </Tab>
+        </Tabs>
+    </Page>
+    );
 }
+
+export default Home;
