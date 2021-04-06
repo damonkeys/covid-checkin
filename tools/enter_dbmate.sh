0a9de20af786ea2cd483e22006c549abc9fe963d
Sven Hohlfeld <sho1975@gmail.com>
2021-04-06 23:46:38 +0200

    fix: use natje and correct network

--- tools/enter_dbmate.sh
+++ tools/enter_dbmate.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run --name chckr_dbmate --rm -it -v "$(pwd)/db:/db" --network=chckr_chckr_default chckr/dbmate
+docker run --name chckr_dbmate --rm -it -v "$(pwd)/db:/db" --network=chckr_default ${{ secrets.REGISTRY_SERVER }}/chckr/dbmate
